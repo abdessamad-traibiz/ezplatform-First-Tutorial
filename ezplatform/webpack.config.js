e1c292ec081c38abe75549daa42a7bb0f6d62ca4
abdessamad-traibiz <abdessamad.traibiz@gmail.com>
2023-04-13 09:05:47 +0000

    first commit

---
+++ ezplatform/webpack.config.js
@@ -0,0 +1,48 @@
+const Encore = require('@symfony/webpack-encore');
+const path = require('path');
+const getEzConfig = require('./ez.webpack.config.js');
+const eZConfigManager = require('./ez.webpack.config.manager.js');
+const eZConfig = getEzConfig(Encore);
+const customConfigs = require('./ez.webpack.custom.configs.js');
+
+Encore.reset();
+Encore.setOutputPath('public/assets/build')
+    .setPublicPath('/assets/build')
+    .enableSassLoader()
+    .enableReactPreset()
+    .enableSingleRuntimeChunk()
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]',
+        pattern: /\.(png|svg)$/
+    });
+
+// Welcome page stylesheets
+Encore.addEntry('welcome_page', [
+    path.resolve(__dirname, './assets/scss/welcome-page.scss'),
+]);
+
+// Put your config here.
+Encore.addEntry('app_js', [
+    path.resolve(__dirname, './assets/app.js'),
+]);
+//Configuring Webpack¶
+Encore
+    .addStyleEntry('tutorial', [
+        path.resolve(__dirname, './assets/css/normalize.css'),
+        path.resolve(__dirname, './assets/css/bootstrap.min.css'),
+        path.resolve(__dirname, './assets/css/bootstrap-theme.css'),
+        path.resolve(__dirname, './assets/css/style.css')
+    ])
+    .addEntry('tutorial-js', [
+        path.resolve(__dirname, './assets/js/bootstrap.min.js')
+    ]);
+Encore.addEntry('app_styles', [
+    path.resolve(__dirname, './assets/styles/app.css'),
+]);
+
+const projectConfig = Encore.getWebpackConfig();
+module.exports = [ eZConfig, ...customConfigs, projectConfig ];
+
+// uncomment this line if you've commented-out the above lines
+// module.exports = [ eZConfig, ...customConfigs ];
